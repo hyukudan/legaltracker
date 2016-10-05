4fd638ee65be07c8c4693af3300fdf131c62c414
hyukudan <sergioc@derechonntt.com>
2016-10-05 18:04:58 +0200

    Legal Tracker

--- tracker/settings.py
+++ tracker/settings.py
@@ -10,8 +10,13 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+
+# settings.py
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_ROOT = os.path.join(
+    os.path.dirname(BASE_DIR), "deployment", "media")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
